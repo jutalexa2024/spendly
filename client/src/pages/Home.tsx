5642f2c98b7ec44f67e7a63021d913fb05d75a22
laneman952 <kyle.lane1992@yahoo.com>
2025-03-31 22:24:47 -0400

    updated Home.tsx

---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Spendly</h1>
+      <p>
+        Having trouble keeping up with your subscriptions or what bills are due and when? 
+        If this is you, then you have come to the right place! Spendly is a simple and easy to use app that helps you keep track of your subscriptions and bills. 
+        You can add, edit, and delete subscriptions and bills, and you can also see a list of all your subscriptions and bills in one place. 
+        Spendly is the perfect app for anyone who wants to take control of their finances and stay on top of their bills.
+      </p>
+      <ul>
+        <li><Link to="/about">About</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
