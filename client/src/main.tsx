4da11bb3e4fc39e36b5b93a4882504f0c96c67a9
jbarry89 <joelbarry8907@hotmail.com>
2025-03-31 22:26:21 -0400

    Added content to be able to view each page content.

---
+++ client/src/main.tsx
@@ -0,0 +1,65 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+
+// import AboutPage from './pages/About.tsx'
+// import Billpage from './pages/Bills.tsx';
+// import Dashboard from './pages/Dashboard.tsx';
+import LoginPage from './pages/Login';
+// import SignUpPage from './pages/Signup.tsx';
+// import Subscription from './pages/Subscription.tsx';
+// import UserPreference from './pages/UserPreferences.tsx'
+import App from './App';
+// import HomePage from './pages/Home';
+import ErrorPage from './pages/ErrorPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+    //   {
+    //     path: "/",
+    //     element: <HomePage />,
+    //   },
+    //   {
+    //     path: "/about",
+    //     element: <AboutPage />,
+    //   },
+    //   {
+    //     path: "/dashboard",
+    //     element: <Dashboard />,
+    //   },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    //   {
+    //     path: "/signup",
+    //     element: <SignUpPage />,
+    //   },
+    //   {
+    //     path: "/subscription",
+    //     element: <Subscription />,
+    //   },
+    //   {
+    //     path: "/bills",
+    //     element: <Billpage />,
+    //   },
+    //   {
+    //     path: "/usersettings",
+    //     element: <UserPreference />,
+    //   },
+      
+    ],
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if(rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <RouterProvider router={router} />
+  );
+}
