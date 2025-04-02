5b810f4ed994957d7f6ae7a6d9e0229032c12f8b
jbarry89 <joelbarry8907@hotmail.com>
2025-04-02 08:22:28 -0400

    Added navbar links and styling

--- client/src/components/nav/index.tsx
+++ client/src/components/nav/index.tsx
@@ -1,21 +1,17 @@
-import { Link, useLocation } from "react-router-dom";
-import "../../styles/nav.css";
+import { Link } from "react-router-dom";
+import "../../styles/nav.css"; // Create this file for styling
 
-const Navbar = () => {
-  const currentPage = useLocation();
-  const hideHeader = currentPage.pathname === "/login" || currentPage.pathname === "/signup";
+const NavLinks = () => {
   return (
-    <nav className="layout">
-      <h1 className="logo">SubTrack</h1> {/* App name/logo */}
-      <ul className="nav-links">
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/bills">Bills</Link></li>
-        <li><Link to="/subscription">Subscriptions</Link></li>
-        <li><Link to="/usersettings">Settings</Link></li>
-        <li><Link to="/login" className="login-btn">Login</Link></li>
-      </ul>
+    <nav className="nav-links">
+      <Link to="/">Home</Link>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/subscription">Subscription</Link>
+      <Link to="/bills">Bills</Link>
+      <Link to="/login">Login</Link>
+
     </nav>
   );
 };
-export default Navbar;
+
+export default NavLinks;
