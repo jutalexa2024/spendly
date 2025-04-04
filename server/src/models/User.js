472c595873e908c28215d6cc82bc46c2da2e9d37
jutalexa2024 <jajustinalexander@gmail.com>
2025-04-03 21:29:13 -0500

    server and package update

---
+++ server/src/models/User.js
@@ -0,0 +1,38 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const bcryptjs_1 = __importDefault(require("bcryptjs"));
+const UserSchema = new mongoose_1.Schema({
+    user_id: { type: Number, required: true, unique: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true },
+}, { timestamps: true });
+UserSchema.methods.isCorrectPassword = function (password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        return bcryptjs_1.default.compare(password, this.password);
+    });
+};
+UserSchema.pre('save', function (next) {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (this.isNew || this.isModified('password')) {
+            const saltRounds = 10;
+            this.password = yield bcryptjs_1.default.hash(this.password, saltRounds);
+        }
+        next();
+    });
+});
+const User = (0, mongoose_1.model)('User', UserSchema);
+exports.default = User;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,32 +2,27 @@ import express from 'express';
 import http from 'http';
 import connectDB from './config/connection';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import { expressMiddleware, ExpressContextFunctionArgument } from '@apollo/server/express4';
+
 import { typeDefs, resolvers } from './schemas/index';
 import { User, Bill, Subscription } from './models';
 import { authenticateToken } from './utils/auth';
 
-
-
-
 const startApolloServer = async () => {
   await connectDB();
-  
-  
+
   const app = express();
   const httpServer = http.createServer(app);
-  
-  
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
-  
+
   await server.start();
-  
+
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  
 
   app.use(
     '/graphql',
@@ -38,16 +33,12 @@ const startApolloServer = async () => {
       },
     }) as unknown as express.RequestHandler
   );
-  
-  
 
-  
   const PORT = process.env.PORT || 3001;
-  await new Promise<void>(resolve => httpServer.listen({ port: PORT }, resolve));
-  
+  await new Promise<void>((resolve) => httpServer.listen({ port: PORT }, resolve));
+
   console.log(`API server running on port ${PORT}!`);
   console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
 };
 
-
-startApolloServer().catch(err => console.error('Error starting server:', err));
+startApolloServer().catch((err) => console.error('Error starting server:', err));
