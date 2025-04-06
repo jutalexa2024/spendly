e167269fb2afc30a58336b6cf616be5c1ac7edbb
nolan MARSHKE <nmarshke@gmail.com>
2025-04-06 18:50:18 -0400

    adding apolloClient

--- client/src/utils/apolloClient.ts
+++ client/src/utils/apolloClient.ts
@@ -5,3 +5,14 @@ import { onError } from '@apollo/client/link/error';
 const httpLink = createHttpLink({
     uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:3001/graphql',
   });
+
+
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.error(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      );
+    if (networkError) console.error(`[Network error]: ${networkError}`);
+  });
