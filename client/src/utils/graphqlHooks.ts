0b4890f15169760f1c8561f2a6a49f0f233a29ce
nolan MARSHKE <nmarshke@gmail.com>
2025-04-06 18:53:59 -0400

    adding Hooks

--- client/src/utils/graphqlHooks.ts
+++ client/src/utils/graphqlHooks.ts
@@ -2,3 +2,17 @@ import { useState } from 'react';
 import { useMutation, useQuery } from '@apollo/client';
 import { gql } from '@apollo/client';
 import authService from './auth';
+
+
+const LOGIN_USER = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
