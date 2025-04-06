aa72aa6c54f378ac60ea2cb95fe4b0b8336094ac
laneman952 <kyle.lane1992@yahoo.com>
2025-04-06 00:21:48 -0400

    updated home.css with gradient, images, and layout format. Updated home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import "../styles/home.css";
+import previewImage from "../../assets/Spendly-subscription-preview.png";
 
 
 const Home = () => {
@@ -22,15 +23,18 @@ const Home = () => {
       </ul>
       </div>
       <div className="right-column">
-        <img src="https://via.placeholder.com/400" alt="Placeholder" />
-        <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://spendly.app" alt="Placeholder" />
+        <img 
+        src="Spendly-subscription-preview.png" alt="Preview Image" className="preview-image" />
+        <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=https://spendly.app" alt="QR Code" className="qr-code" />
         <p>Download This App</p>
+        <div className="store-buttons">
         <button onClick={() => console.log('App Store button clicked')}>
           <img src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg" alt="App Store" />
         </button>
         <button onClick={() => console.log('Google Play button clicked')}>
           <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Google Play" />
         </button>
+        </div>
       </div>
       </section>
       </header>
