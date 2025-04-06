e098add414b76092daf5712b132abff88399c234
nolan MARSHKE <nmarshke@gmail.com>
2025-04-06 18:49:22 -0400

    adding apolloClient

---
+++ client/src/utils/apolloClient.ts
@@ -0,0 +1,3 @@
+import { ApolloClient, InMemoryCache, createHttpLink, from } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { onError } from '@apollo/client/link/error';
