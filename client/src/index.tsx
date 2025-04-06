058c48f8782c0b83f46d305d8bd5fc9097b79ee4
nolan MARSHKE <nmarshke@gmail.com>
2025-04-06 18:53:00 -0400

    adding apolloClient

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -12,9 +12,14 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
