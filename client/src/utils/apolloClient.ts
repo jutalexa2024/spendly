31b611bf924026cecc17be8ed4c95cff86ec6492
nolan MARSHKE <nmarshke@gmail.com>
2025-04-06 18:49:51 -0400

    adding apolloClient

--- client/src/utils/apolloClient.ts
+++ client/src/utils/apolloClient.ts
@@ -1,3 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink, from } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
+
+const httpLink = createHttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_URI || 'http://localhost:3001/graphql',
+  });
