2e6ec792e1cc2c5aee6c19a2b424bbeb26d142a2
Kyle Code <kyle.lane1992@yahoo.com>
2025-04-07 20:12:40 -0400

    updated the footer

---
+++ client/src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+import "../../styles/layout.css";
+
+<footer className="footer">
+    <div className="footer-content">
+        <p>© 2025 Spendly. All rights reserved.</p>
+        <div className="social-icons">
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"/>
+            <img src="https://via.placeholder.com/24" alt="Facebook" />
+        </div>
+    </div>
+</footer>
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import "../styles/home.css";
-import previewImage from "../../assets/Spendly-subscription-preview.png";
 
 
 const Home = () => {
