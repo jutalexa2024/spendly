38c845fab35ee0ef55ace10a143544ba39900641
Kyle Code <kyle.lane1992@yahoo.com>
2025-04-09 19:22:21 -0400

    updated home page paragraph

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -11,10 +11,7 @@ const Home = () => {
             <div className="left-column">
       <h1 className="welcome-title">Welcome to Spendly</h1>
       <p>
-        Having trouble keeping up with your subscriptions or what bills are due and when? 
-        If this is you, then you have come to the right place! Spendly is a simple and easy to use app that helps you keep track of your subscriptions and bills. 
-        You can add, edit, and delete subscriptions and bills, and you can also see a list of all them in one place. 
-        Spendly is the perfect app for anyone who wants to take control of their finances and stay on top of their bills.
+        Take control of your subscriptions and bills all in one place! Spendly maked it easy to track, manage, and stay on top of what you pay, so you can focus on what matters most!
       </p>
       </div>
       <div className="right-column">
