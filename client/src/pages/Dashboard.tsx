9eedc82e2d7124a4d8d82d296346239abc0424eb
jbarry89 <joelbarry8907@hotmail.com>
2025-04-09 02:20:18 -0400

    Added the Dashboard page

---
+++ client/src/pages/Dashboard.tsx
@@ -0,0 +1,19 @@
+import "../styles/dashboard.css";
+
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard-wrapper">
+    <div className="dashboard-container">
+      <h1>About Spendly</h1>
+      <p>Having trouble keeping up with your subscriptions or what bills are due and when? 
+        If this is you, then you have come to the right place! Spendly is a simple and easy to use app that helps you keep track of your subscriptions and bills. 
+        You can add, edit, and delete subscriptions and bills, and you can also see a list of all them in one place. 
+        Spendly is the perfect app for anyone who wants to take control of their finances and stay on top of their bills.
+        </p>
+        </div>
+    </div>
+  );
+}
+
+export default Dashboard;
