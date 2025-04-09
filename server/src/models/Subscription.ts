23f75eb436e06276240b03caeacf5e462c257269
nolan MARSHKE <nmarshke@gmail.com>
2025-04-08 23:09:05 -0400

    working on Subscription

--- server/src/models/Subscription.ts
+++ server/src/models/Subscription.ts
@@ -1,26 +1,56 @@
 import { Schema, model, Document } from 'mongoose';
 
-
 export interface ISubscription extends Document {
   user_id: Schema.Types.ObjectId;
   username: string;
-  email: string;
-  password: string;
+  name: string;
+  status: string;
+  cycle: string;
+  cost: number;
+  paymentStatus: string;
+  dueDate: string;
 }
 
 const subscriptionSchema = new Schema<ISubscription>(
   {
-  user_id:{type: Schema.Types.ObjectId,
-  ref: 'User',
-  unique: true},
-
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
-  
-}, 
-
-{ timestamps: true });
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    username: { 
+      type: String, 
+      required: true 
+    },
+    name: { 
+      type: String, 
+      required: true 
+    },
+    status: { 
+      type: String, 
+      required: true,
+      enum: ['Active', 'Inactive']
+    },
+    cycle: { 
+      type: String, 
+      required: true,
+      enum: ['Monthly', 'Annually']
+    },
+    cost: { 
+      type: Number, 
+      required: true 
+    },
+    paymentStatus: {
+      type: String,
+      required: true,
+      enum: ['Paid', 'Unpaid']
+    },
+    dueDate: {
+      type: String,
+      required: true
+    }
+  }, 
+  { timestamps: true }
+);
 
 const Subscription = model<ISubscription>('Subscription', subscriptionSchema);
 
