36bcc1793ea345fe9807f0ff249fc9f8244681b6
nolan MARSHKE <nmarshke@gmail.com>
2025-04-08 20:56:46 -0400

    finally got graphql sandbox to work

---
+++ client/src/pages/UserPreferences.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserPreferences: React.FC = () => {
+  return (
+    <div>
+      <h1>User Preferences</h1>
+      <p>Manage your account settings here.</p>
+    </div>
+  );
+};
+
+export default UserPreferences;
